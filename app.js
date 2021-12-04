3fdae9e1481c77ed089d76a0c45983cc23902374
kévin <kremadnia@gmail.com>
2021-12-04 12:10:04 +0100

    ADD: a function for check user's answser & a function for display the result of the quizz & 2 function for reset quizz and handle style on buttons

--- app.js
+++ app.js
@@ -43,3 +43,83 @@ const correctAnswer = ['c','a','b','a','c'];
 const emojis = ['✔️','✨','👀','😭','👎'];
 let userAnswer = [];
 let goodAnswer = 0;
+
+// Function
+const radioCheckedHandler = (userArray) => {
+  // create array and push all checked inputs
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked === true) {
+      userArray.push(inputs[i].value);
+    }
+  }
+}
+
+const checkAnswer = () => {
+  // test the user answer with the correct answer arrays
+  for (let i = 0; i < correctAnswer.length; i++) {
+    if (userAnswer[i] === correctAnswer[i]) {
+      // and if it's ok put the background color to lightgreen
+      questions[i].style.backgroundColor = '#b6ff9b';
+      goodAnswer++;
+    } else {
+      // if it's not, put the background to lightred
+      questions[i].style.backgroundColor = '#f86464';
+    }
+  }
+}
+
+const resultDisplay = () => {
+  // before display result, disable all inputs
+  for (let y = 0; y < inputs.length; y++) {
+    inputs[y].disabled = true;
+  }
+  // & the submit button
+  submit.disabled = true;
+  // for each case we have a different sentence's result
+  switch(goodAnswer) {
+    case 0:
+      results.textContent = `Sans déconner ... ${goodAnswer}/5, pas d'emojis pour toi !`;
+      break;
+    case 1:
+      results.textContent = "Fait un effort ... " + emojis[4];
+      break;
+    case 2:
+      results.textContent = "Tu aurais pu mieux faire ... " + emojis[3];
+      break;
+    case 3:
+      results.textContent = "Dommage il t'en manque deux ... " + emojis[2];
+      break;
+    case 4:
+      results.textContent = "Pas mal, tu y était presque ... " + emojis[1];
+      break;
+    case 5:
+      results.textContent = "Le sans-fautes, Bravo à toi ... " + emojis[0];
+      break;
+    default:
+      results.textContent = "Il y a eu un problèmes avec le traitement du résultat";
+  }
+}
+
+const waitReset = () => {
+  // disable event on submit button
+  submit.style.pointerEvents = "none";
+  reset.classList.add('restart-animation');
+}
+
+const resetQuizz = () => {
+  // reset all data
+  goodAnswer = 0;
+  userAnswer = [];
+  // & put the background color of question to white
+  for (let question of questions) {
+    question.style.backgroundColor = 'white';
+  }
+  // & activate all input
+  for (let y = 0; y < inputs.length; y++) {
+    inputs[y].disabled = false;
+  }
+  // & active the submit button
+  submit.disabled = false;
+  // reset the result sentence
+  results.textContent = "";
+}
